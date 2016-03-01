f61b23c428d2b25b3ceed985c3ace8ac2e0cb7de
Darren Cauthon <darren@cauthon.com>
2016-03-01 15:13:47 -0600

    Get an email to send.

---
+++ src/SparkPost.Tests/TransmissionTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace SparkPost.Tests
+{
+    [TestFixture]
+    public class TransmissionTests
+    {
+        [Test]
+        public void FireAnEmail()
+        {
+            var transmission = new Transmission();
+            var result = transmission.FireAnEmail("");
+            result.Wait();
+        }
+    }
+}
---
+++ src/SparkPost/Transmission.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Dynamic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SparkPost
+{
+    public class Transmission
+    {
+        public async Task<HttpResponseMessage> FireAnEmail(string apiKey)
+        {
+            using (var client = new HttpClient())
+            {
+                // New code:
+                client.BaseAddress = new Uri("https://api.sparkpost.com");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Add("Authorization", apiKey);
+
+                dynamic content = new ExpandoObject();
+                content.from = "testing@sparkpostbox.com";
+                content.subject = "Oh hey";
+                content.text = "Testing";
+
+                dynamic recipient = new ExpandoObject();
+                recipient.address = "darren@cauthon.com";
+
+                dynamic data = new ExpandoObject();
+                data.content = content;
+                data.recipients = new[] {recipient};
+
+                var theString = JsonConvert.SerializeObject(data);
+                var stringContent = new StringContent(JsonConvert.SerializeObject(data));
+
+                return await client.PostAsync("api/v1/transmissions", stringContent);
+            }
+        }
+    }
+}

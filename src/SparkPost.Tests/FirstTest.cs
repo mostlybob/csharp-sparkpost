9cb642b89b128c22d30b3650f4e229152d09dc65
Darren Cauthon <darren@cauthon.com>
2016-03-01 14:57:12 -0600

    Drop in the first tests.

---
+++ src/SparkPost.Tests/FirstTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Should;
+
+namespace SparkPost.Tests
+{
+    [TestFixture]
+    public class FirstTest
+    {
+        [Test]
+        public void True()
+        {
+            1.ShouldEqual(1);
+        }
+    }
+}

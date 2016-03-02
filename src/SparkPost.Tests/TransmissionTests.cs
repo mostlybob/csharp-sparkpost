4151abcc882cb85d437699c5ac92f8448d96e327
Darren Cauthon <darren@cauthon.com>
2016-03-02 08:49:24 -0600

    Test these changes I made.

---
+++ src/SparkPost.Tests/TransmissionTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Should;
+
+namespace SparkPost.Tests
+{
+    public class TransmissionTests
+    {
+        [TestFixture]
+        public class ConstructorTests
+        {
+            [SetUp]
+            public void Setup()
+            {
+                transmission = new Transmission();
+            }
+
+            private Transmission transmission;
+
+            [Test]
+            public void It_should_not_be_missing_a_recipients_list()
+            {
+                transmission.Recipients.ShouldNotBeNull();
+            }
+
+            [Test]
+            public void It_should_not_be_missing_content()
+            {
+                transmission.Content.ShouldNotBeNull();
+            }
+
+            [Test]
+            public void It_should_not_be_missing_metadata()
+            {
+                transmission.Metadata.ShouldNotBeNull();
+            }
+
+            [Test]
+            public void It_should_not_be_missing_substition_data()
+            {
+                transmission.SubstitutionData.ShouldNotBeNull();
+            }
+        }
+    }
+}

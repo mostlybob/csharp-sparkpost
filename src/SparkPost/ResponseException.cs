10931152cd07d5e905b37be577edab38373e1242
Darren Cauthon <darren@cauthon.com>
2016-03-03 11:33:58 -0600

    Forgot to add these files.

---
+++ src/SparkPost/ResponseException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SparkPost
+{
+    public class ResponseException : Exception
+    {
+        private readonly Response response;
+
+        public ResponseException(Response response)
+        {
+            this.response = response;
+        }
+
+        public override string Message =>
+            JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content)["errors"];
+    }
+}

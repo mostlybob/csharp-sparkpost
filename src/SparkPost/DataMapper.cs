750c61e8f9264b9d887c9f573a79f41ce0721340
Darren Cauthon <darren@cauthon.com>
2016-03-10 16:59:27 -0600

    Create a method that will convert objects to their proper API form.

--- src/SparkPost/DataMapper.cs
+++ src/SparkPost/DataMapper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace SparkPost
 {
@@ -39,12 +40,7 @@ namespace SparkPost
 
         public virtual IDictionary<string, object> ToDictionary(Address address)
         {
-            return RemoveNulls(new Dictionary<string, object>
-            {
-                ["email"] = address.Email,
-                ["name"] = address.Name,
-                ["header_to"] = address.HeaderTo
-            });
+            return WithCommonConventions(address);
         }
 
         public virtual IDictionary<string, object> ToDictionary(Options options)
@@ -117,6 +113,29 @@ namespace SparkPost
             foreach (var key in dictionary.Keys.Where(k => dictionary[k] != null))
                 newDictionary[key] = dictionary[key];
             return newDictionary;
+        }
+
+        private IDictionary<string, object> WithCommonConventions(object target)
+        {
+            var results = new Dictionary<string, object>();
+            foreach (var property in target.GetType().GetProperties())
+                results[ToSnakeCase(property.Name)] = property.GetValue(target);
+            return RemoveNulls(results);
+        }
+
+        private string ToSnakeCase(string input)
+        {
+            var regex = new Regex("[A-Z]");
+
+            var matches = regex.Matches(input);
+
+            for (var i = 0; i < matches.Count; i++)
+                input = input.Replace(matches[i].Value, "_" + matches[i].Value.ToLower());
+
+            if (input.StartsWith("_"))
+                input = input.Substring(1, input.Length - 1);
+
+            return input;
         }
     }
 }

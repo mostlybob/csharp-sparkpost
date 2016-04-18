f387998bf8b35242149e7e1d928973c5f05c42e2
Darren Cauthon <darren@cauthon.com>
2016-04-17 19:15:14 -0500

    Move these results to their own spot.

--- src/SparkPost/ListWebhookResponse.cs
+++ src/SparkPost/ListWebhookResponse.cs
@@ -1,9 +1,41 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace SparkPost
 {
     public class ListWebhookResponse : Response
     {
         public IEnumerable<Webhook> Webhooks { get; set; }
+
+        public static ListWebhookResponse CreateFromResponse(Response result)
+        {
+            var response = new ListWebhookResponse();
+            LeftRight.SetValuesToMatch(response, result);
+
+            var results = JsonConvert.DeserializeObject<dynamic>(response.Content).results;
+            var webhooks = new List<Webhook>();
+            foreach(var r in results)
+            {
+                var events = new List<string>();
+                foreach(var i in r.events)
+                    events.Add(i.ToString());
+                var webhook = new Webhook
+                {
+                    Id = r.id,
+                    Name = r.name,
+                    Target = r.target,
+                    Events = events,
+                    AuthType = r.auth_type,
+                    AuthRequestDetails = r.auth_request_details,
+                    AuthCredentials = r.auth_credentials,
+                    AuthToken = r.auth_token,
+                    LastSuccessful = r.last_successful,
+                    LastFailure = r.last_failure
+                };
+                webhooks.Add(webhook);
+            }
+            response.Webhooks = webhooks;
+            return response;
+        }
     }
 }
--- src/SparkPost/Webhooks.cs
+++ src/SparkPost/Webhooks.cs
@@ -40,34 +40,7 @@ namespace SparkPost
             var result = await requestSender.Send(request);
             if (result.StatusCode != HttpStatusCode.OK) throw new ResponseException(result);
 
-            var response = new ListWebhookResponse();
-            LeftRight.SetValuesToMatch(response, result);
-
-            var results = JsonConvert.DeserializeObject<dynamic>(response.Content).results;
-            var webhooks = new List<Webhook>();
-            foreach(var r in results)
-            {
-                var events = new List<string>();
-                foreach(var i in r.events)
-                    events.Add(i.ToString());
-                var webhook = new Webhook
-                {
-                    Id = r.id,
-                    Name = r.name,
-                    Target = r.target,
-                    Events = events,
-                    AuthType = r.auth_type,
-                    AuthRequestDetails = r.auth_request_details,
-                    AuthCredentials = r.auth_credentials,
-                    AuthToken = r.auth_token,
-                    LastSuccessful = r.last_successful,
-                    LastFailure = r.last_failure
-                };
-                webhooks.Add(webhook);
-            }
-            response.Webhooks = webhooks;
-
-            return response;
+            return ListWebhookResponse.CreateFromResponse(result);
         }
 
         public async Task<Response> Create(Webhook webhook)

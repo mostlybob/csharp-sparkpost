9c6fe284ce20da35b8ad13e85ec78b2a190252fd
Darren Cauthon <darren@cauthon.com>
2016-04-21 07:04:46 -0500

    Provide a method that allows the overriding of the http client building.

--- src/SparkPost/Client.cs
+++ src/SparkPost/Client.cs
@@ -1,15 +1,20 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 
 namespace SparkPost
 {
     public class Client : IClient
     {
+        private Func<HttpClient> httpClientBuilder;
+
         public Client(string apiKey, string apiHost = "https://api.sparkpost.com")
         {
             ApiKey = apiKey;
             ApiHost = apiHost;
             Transmissions = new Transmissions(this, new RequestSender(this), new DataMapper(Version));
             Suppressions = new Suppressions(this, new RequestSender(this), new DataMapper());
+
+            httpClientBuilder = () => new HttpClient();
         }
 
         public string ApiKey { get; set; }
@@ -21,7 +26,12 @@ namespace SparkPost
 
         internal HttpClient CreateANewHttpClient()
         {
-            return new HttpClient();
+            return httpClientBuilder();
+        }
+
+        public void BuildHttpClientsUsing(Func<HttpClient> httpClient)
+        {
+            this.httpClientBuilder = httpClient;
         }
     }
 }

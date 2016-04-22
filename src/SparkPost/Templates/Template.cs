ab2c50b2824fa35a0a40813f544ad7036a70ca41
jamesoflol <james.lol@hale.com.au>
2016-04-22 10:23:52 +1000

    Initial add of templates dir files

---
+++ src/SparkPost/Templates/Template.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace SparkPost
+{
+    public class Template
+    {
+        public Template()
+        {
+            //Content = new TemplateContent();
+            Content = new TemplateContent();
+            Options = new TemplateOptions();
+        }
+
+        public string Id { get; set; }
+        //public TemplateContent Content { get; set; }
+        public TemplateContent Content { get; set; }
+        public bool Published { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TemplateOptions Options { get; set; }
+
+    }
+}
---
+++ src/SparkPost/Templates/Templates.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace SparkPost
+{
+    public class Templates : ITemplates
+    {
+        private readonly Client client;
+        private readonly RequestSender requestSender;
+        private readonly DataMapper dataMapper;
+
+        public Templates(Client client, RequestSender requestSender, DataMapper dataMapper)
+        {
+            this.client = client;
+            this.requestSender = requestSender;
+            this.dataMapper = dataMapper;
+        }
+
+        public async Task<CreateTemplateResponse> Create(Template template)
+        {
+            var request = new Request
+            {
+                Url = $"api/{client.Version}/templates",
+                Method = "POST",
+                Data = dataMapper.ToDictionary(template)
+            };
+
+            var response = await requestSender.Send(request);
+            if (response.StatusCode != HttpStatusCode.OK) throw new ResponseException(response);
+
+            var results = JsonConvert.DeserializeObject<dynamic>(response.Content).results;
+            return new CreateTemplateResponse()
+            {
+                Id = results.id,
+                //ReasonPhrase = response.ReasonPhrase,
+                //StatusCode = response.StatusCode,
+                //Content = response.Content,
+            };
+        }
+    }
+}

91f304efc190b3e435ed000028594aec0bbb4fd6
Darren Cauthon <darren@cauthon.com>
2016-04-26 16:56:58 -0500

    Test the async sender.

---
+++ src/SparkPost.Tests/SyncRequestSenderTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using AutoMoq.Helpers;
+using NUnit.Framework;
+using Should;
+
+namespace SparkPost.Tests
+{
+    public class SyncRequestSenderTests
+    {
+        [TestFixture]
+        public class SendTests : AutoMoqTestFixture<SyncRequestSender>
+        {
+            [Test]
+            public void It_should_return_the_result_from_the_parent_request_sender()
+            {
+                var request = new Request();
+                var response = new Response();
+
+                Mocked<IRequestSender>().Setup(x => x.Send(request)).Returns(Task.FromResult(response));
+
+                var result = Subject.Send(request);
+
+                result.Result.ShouldBeSameAs(response);
+            }
+        }
+    }
+}

c8f4c28a7713e725d19b12ae46211482114c2bc1
Darren Cauthon <darren@cauthon.com>
2016-04-28 09:28:26 -0500

    Cleanup.

--- src/SparkPost/DataMapper.cs
+++ src/SparkPost/DataMapper.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Reflection;
 using System.Text.RegularExpressions;
 using SparkPost.ValueMappers;
 

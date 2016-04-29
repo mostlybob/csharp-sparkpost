c54a6d624f8ed464c3e5488d369b2e6d78594111
Darren Cauthon <darren@cauthon.com>
2016-04-29 07:05:09 -0500

    Add some tests around this.

---
+++ src/SparkPost.Tests/Utilities/SnakeCaseTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Should;
+using SparkPost.Utilities;
+
+namespace SparkPost.Tests.Utilities
+{
+    [TestFixture]
+    public class SnakeCaseTests
+    {
+        [Test]
+        public void It_should_convert_things_to_snake_case()
+        {
+            SnakeCase.Convert("T").ShouldEqual("t");
+            SnakeCase.Convert("Test").ShouldEqual("test");
+            SnakeCase.Convert("TEST").ShouldEqual("t_e_s_t");
+            SnakeCase.Convert("JohnGalt").ShouldEqual("john_galt");
+        }
+    }
+}

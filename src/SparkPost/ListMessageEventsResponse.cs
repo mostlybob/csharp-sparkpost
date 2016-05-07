c23ddaea27b1a3f3393274762ca12e7b8d395cd9
Darren Cauthon <darren@cauthon.com>
2016-05-07 11:06:38 -0500

    Do not default these to nil.

--- src/SparkPost/ListMessageEventsResponse.cs
+++ src/SparkPost/ListMessageEventsResponse.cs
@@ -4,6 +4,12 @@ namespace SparkPost
 {
     public class ListMessageEventsResponse : Response
     {
+        public ListMessageEventsResponse()
+        {
+            MessageEvents = new MessageEvent[] {};
+            Links = new PageLink[] {};
+        }
+
         public IEnumerable<MessageEvent> MessageEvents { get; set; }
 
         public IList<PageLink> Links { get; set; }

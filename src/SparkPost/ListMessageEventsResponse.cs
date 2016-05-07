1b28a9f537f8f20305c7a70bb55937ac09917378
Darren Cauthon <darren@cauthon.com>
2016-05-07 10:58:17 -0500

    Do not need this attribute.

--- src/SparkPost/ListMessageEventsResponse.cs
+++ src/SparkPost/ListMessageEventsResponse.cs
@@ -1,11 +1,9 @@
-using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace SparkPost
 {
     public class ListMessageEventsResponse : Response
     {
-        [JsonProperty("results")]
         public IEnumerable<MessageEvent> MessageEvents { get; set; }
 
         public IList<PageLink> Links { get; set; }

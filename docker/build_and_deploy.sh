d6661b3272eeee87756642bdefa73331f5400b65
Darren Cauthon <darren@cauthon.com>
2016-05-30 19:45:24 -0500

    Explain the funny business.

--- docker/build_and_deploy.sh
+++ docker/build_and_deploy.sh
@@ -7,6 +7,11 @@ nuget pack SparkPost/SparkPost.nuspec -Prop Configuration=Release
 
 PACKAGE=$(ls *.nupkg)
 
+# This will unzip the nupkg, then zip it back up.
+# This is done to some issue with the nuget command
+# line app creating nupkg files that cannot be
+# used by Visual Studio. 
+# https://github.com/NuGet/Home/issues/2833
 mv $PACKAGE file.zip
 mkdir stuff
 cd stuff

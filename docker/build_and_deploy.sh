fe4e5ddc99f8ec5a79d9ab8d108f4a01538a3a89
Darren Cauthon <darren@cauthon.com>
2016-05-30 19:10:16 -0500

    Try zipping the nupkg myself.

--- docker/build_and_deploy.sh
+++ docker/build_and_deploy.sh
@@ -1,4 +1,6 @@
 git pull origin master
+git remote update
+git checkout -b figure_this_docker_issue_out origin/figure_this_docker_issue_out
 
 nuget restore
 xbuild /p:Configuration=Release
@@ -7,4 +9,13 @@ nuget pack SparkPost/SparkPost.nuspec -Prop Configuration=Release
 
 PACKAGE=$(ls *.nupkg)
 
-nuget push $PACKAGE $APIKEY -s nuget.org
+mv $PACKAGE file.zip
+mkdir stuff
+cd stuff
+unzip ../file.zip
+rm ../file.zip
+zip -r ../file.zip *
+cd ..
+mv file.zip $PACKAGE
+
+# nuget push $PACKAGE $APIKEY -s nuget.org

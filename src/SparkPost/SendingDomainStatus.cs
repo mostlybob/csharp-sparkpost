93aa9b6fd3f662ca096268983e7df3a92edb1bce
Darren Cauthon <darren@cauthon.com>
2016-07-02 22:57:29 -0500

    Cleanup.

--- src/SparkPost/SendingDomainStatus.cs
+++ src/SparkPost/SendingDomainStatus.cs
@@ -17,25 +17,34 @@ namespace SparkPost
         public ComplianceStatus ComplianceStatus { get; set; }
 
         /// <summary>
-        /// Convert json result form Sparkpost API to SendingDomainStatus.
+        ///     Convert json result form Sparkpost API to SendingDomainStatus.
         /// </summary>
         /// <param name="result">Json result form Sparkpost API.</param>
         /// <returns></returns>
         public static SendingDomainStatus ConvertToSendingDomainStatus(dynamic result)
         {
-            return result != null ? new SendingDomainStatus
-            {
-                OwnershipVerified = result.ownership_verified ?? false,
-                DkimStatus = Enum.Parse(typeof(DkimStatus), (result.dkim_status ?? DkimStatus.Unknowed).ToString(), true),
-                SpfStatus = Enum.Parse(typeof(SpfStatus), (result.spf_status ?? SpfStatus.Unknowed).ToString(), true),
-                AbuseAtStatus = Enum.Parse(typeof(AbuseAtStatus), (result.abuse_at_status ?? AbuseAtStatus.Unknowed).ToString(), true),
-                PostmasterAtStatus = Enum.Parse(typeof(PostmasterAtStatus), (result.postmaster_at_status ?? PostmasterAtStatus.Unknowed).ToString(), true),
-                ComplianceStatus = Enum.Parse(typeof(ComplianceStatus), (result.compliance_status ?? ComplianceStatus.Unknowed).ToString(), true)
-            }
+            return result != null
+                ? new SendingDomainStatus
+                {
+                    OwnershipVerified = result.ownership_verified ?? false,
+                    DkimStatus =
+                        Enum.Parse(typeof (DkimStatus), (result.dkim_status ?? DkimStatus.Unknowed).ToString(), true),
+                    SpfStatus =
+                        Enum.Parse(typeof (SpfStatus), (result.spf_status ?? SpfStatus.Unknowed).ToString(), true),
+                    AbuseAtStatus =
+                        Enum.Parse(typeof (AbuseAtStatus), (result.abuse_at_status ?? AbuseAtStatus.Unknowed).ToString(),
+                            true),
+                    PostmasterAtStatus =
+                        Enum.Parse(typeof (PostmasterAtStatus),
+                            (result.postmaster_at_status ?? PostmasterAtStatus.Unknowed).ToString(), true),
+                    ComplianceStatus =
+                        Enum.Parse(typeof (ComplianceStatus),
+                            (result.compliance_status ?? ComplianceStatus.Unknowed).ToString(), true)
+                }
                 : null;
         }
     }
-    
+
     public enum DkimStatus
     {
         Unverified,
@@ -44,7 +53,7 @@ namespace SparkPost
         Valid,
         Unknowed
     }
-    
+
     public enum SpfStatus
     {
         Unverified,
@@ -53,7 +62,7 @@ namespace SparkPost
         Valid,
         Unknowed
     }
-    
+
     public enum AbuseAtStatus
     {
         Unverified,
@@ -62,7 +71,7 @@ namespace SparkPost
         Valid,
         Unknowed
     }
-    
+
     public enum PostmasterAtStatus
     {
         Unverified,
@@ -71,7 +80,7 @@ namespace SparkPost
         Valid,
         Unknowed
     }
-    
+
     public enum ComplianceStatus
     {
         Pending,

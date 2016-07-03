f4ff30c916102caf69dd4dec1ec6d3005762ace6
Darren Cauthon <darren@cauthon.com>
2016-07-02 22:55:50 -0500

    R# cleanup.

--- src/SparkPost/Dns.cs
+++ src/SparkPost/Dns.cs
@@ -3,21 +3,22 @@
     public class Dns
     {
         public string DkimRecord { get; set; }
-        
+
         public string SpfRecord { get; set; }
-        
+
         public string DkimError { get; set; }
-        
+
         public string SpfError { get; set; }
 
         /// <summary>
-        /// Convert json result form Sparkpost API to Dns.
+        ///     Convert json result form Sparkpost API to Dns.
         /// </summary>
         /// <param name="result">Json result form Sparkpost API.</param>
         /// <returns></returns>
         public static Dns ConvertToDns(dynamic result)
         {
-            return result != null ? new Dns
+            return result != null
+                ? new Dns
                 {
                     DkimError = result.dkim_error,
                     DkimRecord = result.dkim_record,

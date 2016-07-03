8cbdabea1b95c4587e766f708e18d8c1c349f814
Darren Cauthon <darren@cauthon.com>
2016-07-02 22:56:16 -0500

    R# cleanup.

--- src/SparkPost/Dkim.cs
+++ src/SparkPost/Dkim.cs
@@ -1,27 +1,26 @@
-﻿using System;
-
-namespace SparkPost
+﻿namespace SparkPost
 {
     public class Dkim
     {
         public string SigningDomain { get; set; }
-        
+
         public string PrivateKey { get; set; }
-        
+
         public string PublicKey { get; set; }
-        
+
         public string Selector { get; set; }
-        
+
         public string Headers { get; set; }
 
         /// <summary>
-        /// Convert json result form Sparkpost API to Dkim.
+        ///     Convert json result form Sparkpost API to Dkim.
         /// </summary>
         /// <param name="result">Json result form Sparkpost API.</param>
         /// <returns></returns>
         public static Dkim ConvertToDkim(dynamic result)
         {
-            return result != null ? new Dkim
+            return result != null
+                ? new Dkim
                 {
                     SigningDomain = result["public"],
                     PublicKey = result["private"],

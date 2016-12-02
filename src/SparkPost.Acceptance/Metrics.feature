9cd1707fc5668480a60eb6042cf729f4689f122e
Darren Cauthon <darren@cauthon.com>
2016-12-02 07:32:40 -0600

    Add a feature file for metrics, with one working test.

---
+++ src/SparkPost.Acceptance/Metrics.feature
@@ -0,0 +1,8 @@
+﻿Feature: Metrics
+
+Background:
+	Given my api key is 'yyy'
+
+Scenario: Sending a regular email
+	When I query my deliverability
+	Then it should return a 200
---
+++ src/SparkPost.Acceptance/MetricsSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace SparkPost.Acceptance
+{
+    [Binding]
+    public class MetricsSteps
+    {
+        [When(@"I query my deliverability")]
+        public void WhenIQueryMyDeliverability()
+        {
+            var client = ScenarioContext.Current.Get<IClient>();
+            Response response = null;
+            Task.Run(async () =>
+            {
+                response = await client.Metrics.GetDeliverability(new
+                {
+                    from = DateTime.MinValue,
+                    metrics = "count_accepted"
+                });
+            }).Wait();
+
+            ScenarioContext.Current.Set(response);
+        }
+    }
+}

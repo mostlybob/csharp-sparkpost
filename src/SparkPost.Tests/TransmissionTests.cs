683de6046282e23439a3a8d2953fd62aad2ab8f0
Darren Cauthon <darren@cauthon.com>
2016-12-07 20:46:54 -0600

    No need for abbreviation.

--- src/SparkPost.Tests/TransmissionTests.cs
+++ src/SparkPost.Tests/TransmissionTests.cs
@@ -53,91 +53,91 @@ namespace SparkPost.Tests
         [TestFixture]
         public class ParseTests
         {
-            private MailMessage _msg;
-            private Transmission _trans;
+            private MailMessage mailMessage;
+            private Transmission transmission;
 
             [SetUp]
             public void Setup()
             {
-                _msg = new MailMessage();
-                _msg.From = new MailAddress("jim@example.com", "Jim Example");
-                _msg.To.Add(new MailAddress("bob@example.com", "Bob Example"));
-                _msg.CC.Add(new MailAddress("susan@example.com", "Susan Example"));
-                _msg.Bcc.Add(new MailAddress("richard@example.com", "Richard Example"));
-                _msg.ReplyToList.Add(new MailAddress("rebecca@example.com", "Rebecca Example"));
-                _msg.Body = "Unit test message";
-                _msg.Subject = "Test ssubject";
+                mailMessage = new MailMessage();
+                mailMessage.From = new MailAddress("jim@example.com", "Jim Example");
+                mailMessage.To.Add(new MailAddress("bob@example.com", "Bob Example"));
+                mailMessage.CC.Add(new MailAddress("susan@example.com", "Susan Example"));
+                mailMessage.Bcc.Add(new MailAddress("richard@example.com", "Richard Example"));
+                mailMessage.ReplyToList.Add(new MailAddress("rebecca@example.com", "Rebecca Example"));
+                mailMessage.Body = "Unit test message";
+                mailMessage.Subject = "Test ssubject";
 
 
 
-                _trans = new Transmission(_msg);                
+                transmission = new Transmission(mailMessage);                
             }
 
             
             [Test]
             public void From_should_match()
             {
-                Assert.That(_trans.Content.From.Name, Is.EqualTo(_msg.From.DisplayName));
-                Assert.That(_trans.Content.From.Email, Is.EqualTo(_msg.From.Address));
+                Assert.That(transmission.Content.From.Name, Is.EqualTo(mailMessage.From.DisplayName));
+                Assert.That(transmission.Content.From.Email, Is.EqualTo(mailMessage.From.Address));
             }
 
             [Test]
             public void It_should_have_three_recipients()
             {
-                Assert.That(_trans.Recipients.Count, Is.EqualTo(3));
+                Assert.That(transmission.Recipients.Count, Is.EqualTo(3));
             }
 
             [Test]
             public void To_should_match()
             {
-                var to = _trans.Recipients.SingleOrDefault(r => r.Type == RecipientType.To);
+                var to = transmission.Recipients.SingleOrDefault(r => r.Type == RecipientType.To);
                 Assert.That(to, Is.Not.Null);
-                Assert.That(to.Address.Name == _msg.To.First().DisplayName);
-                Assert.That(to.Address.Email == _msg.To.First().Address);
+                Assert.That(to.Address.Name == mailMessage.To.First().DisplayName);
+                Assert.That(to.Address.Email == mailMessage.To.First().Address);
             }
 
             [Test]
             public void Cc_should_match()
             {
-                var cc = _trans.Recipients.SingleOrDefault(r => r.Type == RecipientType.CC);
+                var cc = transmission.Recipients.SingleOrDefault(r => r.Type == RecipientType.CC);
                 Assert.That(cc, Is.Not.Null);
-                Assert.That(cc.Address.Name == _msg.CC.First().DisplayName);
-                Assert.That(cc.Address.Email == _msg.CC.First().Address);
+                Assert.That(cc.Address.Name == mailMessage.CC.First().DisplayName);
+                Assert.That(cc.Address.Email == mailMessage.CC.First().Address);
             }
 
             [Test]
             public void Bcc_should_match()
             {
-                var bcc = _trans.Recipients.SingleOrDefault(r => r.Type == RecipientType.BCC);
+                var bcc = transmission.Recipients.SingleOrDefault(r => r.Type == RecipientType.BCC);
                 Assert.That(bcc, Is.Not.Null);
-                Assert.That(bcc.Address.Name == _msg.Bcc.First().DisplayName);
-                Assert.That(bcc.Address.Email == _msg.Bcc.First().Address);
+                Assert.That(bcc.Address.Name == mailMessage.Bcc.First().DisplayName);
+                Assert.That(bcc.Address.Email == mailMessage.Bcc.First().Address);
             }
 
             [Test]
             public void Replyto_should_match()
             {
-                Assert.That(_trans.Content.ReplyTo, Is.EqualTo(_msg.ReplyToList.First().Address));
+                Assert.That(transmission.Content.ReplyTo, Is.EqualTo(mailMessage.ReplyToList.First().Address));
             }
 
             [Test]
             public void Subject_should_match()
             {
-                Assert.That(_trans.Content.Subject, Is.EqualTo(_msg.Subject));
+                Assert.That(transmission.Content.Subject, Is.EqualTo(mailMessage.Subject));
             }
 
             [Test]
             public void Text_body_should_match()
             {
-                Assert.That(_trans.Content.Text, Is.EqualTo(_msg.Body));
+                Assert.That(transmission.Content.Text, Is.EqualTo(mailMessage.Body));
             }
 
             [Test]
             public void Html_body_should_match()
             {
-                _msg.IsBodyHtml = true;
-                _trans = new Transmission(_msg);
-                Assert.That(_trans.Content.Html, Is.EqualTo(_msg.Body));
+                mailMessage.IsBodyHtml = true;
+                transmission = new Transmission(mailMessage);
+                Assert.That(transmission.Content.Html, Is.EqualTo(mailMessage.Body));
             }
 
             [Test]
@@ -145,22 +145,22 @@ namespace SparkPost.Tests
             {
                 var html = "<p>Html body</p>";
                 var view = AlternateView.CreateAlternateViewFromString(html, null, MediaTypeNames.Text.Html);
-                _msg.AlternateViews.Add(view);
-                _trans = new Transmission(_msg);
+                mailMessage.AlternateViews.Add(view);
+                transmission = new Transmission(mailMessage);
 
-                Assert.That(_trans.Content.Html, Is.EqualTo(html));
+                Assert.That(transmission.Content.Html, Is.EqualTo(html));
             }
 
             [Test]
             public void It_should_use_alternate_text_view()
             {
-                _msg.IsBodyHtml = true;
+                mailMessage.IsBodyHtml = true;
                 var text = "Text body";
                 var view = AlternateView.CreateAlternateViewFromString(text, null, MediaTypeNames.Text.Plain);
-                _msg.AlternateViews.Add(view);
-                _trans = new Transmission(_msg);
+                mailMessage.AlternateViews.Add(view);
+                transmission = new Transmission(mailMessage);
 
-                Assert.That(_trans.Content.Text, Is.EqualTo(text));
+                Assert.That(transmission.Content.Text, Is.EqualTo(text));
             }
 
             [Test]
@@ -169,13 +169,13 @@ namespace SparkPost.Tests
                 var text = "Alternate text";
                 var html = "Alternate html";
                 var view = AlternateView.CreateAlternateViewFromString(text, null, MediaTypeNames.Text.Plain);
-                _msg.AlternateViews.Add(view);
+                mailMessage.AlternateViews.Add(view);
                 view = AlternateView.CreateAlternateViewFromString(html, null, MediaTypeNames.Text.Html);
-                _msg.AlternateViews.Add(view);
-                _trans = new Transmission(_msg);
+                mailMessage.AlternateViews.Add(view);
+                transmission = new Transmission(mailMessage);
 
-                Assert.That(_trans.Content.Text, Is.EqualTo(text));
-                Assert.That(_trans.Content.Html, Is.EqualTo(html));
+                Assert.That(transmission.Content.Text, Is.EqualTo(text));
+                Assert.That(transmission.Content.Html, Is.EqualTo(html));
             }
 
             [Test]
@@ -185,14 +185,14 @@ namespace SparkPost.Tests
                 var name = "foo.txt";
                 var type = "text/plain";
                 var ms = new MemoryStream(Encoding.ASCII.GetBytes(text));
-                _msg.Attachments.Add(new System.Net.Mail.Attachment(ms, name, type));
-                _trans = new Transmission(_msg);
+                mailMessage.Attachments.Add(new System.Net.Mail.Attachment(ms, name, type));
+                transmission = new Transmission(mailMessage);
 
-                Assert.That(_trans.Content.Attachments.Count, Is.EqualTo(1));
-                Assert.That(_trans.Content.Attachments.First().Type, Is.EqualTo(type));
-                Assert.That(_trans.Content.Attachments.First().Name, Is.EqualTo(name));
+                Assert.That(transmission.Content.Attachments.Count, Is.EqualTo(1));
+                Assert.That(transmission.Content.Attachments.First().Type, Is.EqualTo(type));
+                Assert.That(transmission.Content.Attachments.First().Name, Is.EqualTo(name));
 
-                var bytes = Convert.FromBase64String(_trans.Content.Attachments.First().Data);
+                var bytes = Convert.FromBase64String(transmission.Content.Attachments.First().Data);
                 var decoded = Encoding.ASCII.GetString(bytes);
                 Assert.That(decoded, Is.EqualTo(text));
             }
